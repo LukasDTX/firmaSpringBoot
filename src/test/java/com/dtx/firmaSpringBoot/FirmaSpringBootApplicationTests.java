6ade41b701a40952166034406a562e54ccf49276
Lukasz Hacas <lukaszhacas@gmail.com>
2023-01-10 00:24:05 +0100

    initial commit

---
+++ src/test/java/com/dtx/firmaSpringBoot/FirmaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtx.firmaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
